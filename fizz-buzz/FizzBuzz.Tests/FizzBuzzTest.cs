70717a4570b87d7a1fe5a9fcbd38a3b5b2900a04
jmfenoll <jmfenoll@gmail.com>
2026-06-10 17:11:13 +0200

    Exercise 1

--- fizz-buzz/FizzBuzz.Tests/FizzBuzzTest.cs
+++ fizz-buzz/FizzBuzz.Tests/FizzBuzzTest.cs
@@ -1,13 +1,90 @@
+using System;
 using NUnit.Framework;
 
 namespace FizzBuzz.Tests
 {
+    /*
+    Write a program that transform any natural number between 1 and 100 into:
+
+    "Fizz" for multiples of three.
+    "Buzz" for multiples of five.
+    "FizzBuzz" for numbers which are multiples of both three and five.
+    The number as a string in any other case.
+
+    Note: The program should let the user know that it can't process any input that is not valid.
+    
+    ** Behavior: Convert any integer into string
+
+    ** Small behaviors: 
+        if the number is a multiple of 3, return "Fizz"
+        if the number is a multiple of 5, return "Buzz"
+        if the number is a multiple of both 3 and 5, return "FizzBuzz"
+        if the number is not a multiple of 3 or 5, return the number as a string
+        if the input is not a valid integer, return an error message indicating that the input is invalid
+     
+    Input: A string containing a natural number between 1 and 100
+    Output: A string representing the FizzBuzz result or an error message
+
+    Examples:
+    "" -> "Invalid input: Please enter a natural number between 1 and 100."
+    "pepe" -> "Invalid input: Please enter a natural number between 1 and 100."
+    "0" -> "Invalid input: Please enter a natural number between 1 and 100."
+    "101" -> "Invalid input: Please enter a natural number between 1 and 100."
+    "1" -> "1"
+    "2" -> "2"
+    "3" -> "Fizz"
+    "5" -> "Buzz"
+    "15" -> "FizzBuzz"
+    "100" -> "Buzz"
+     */
+
+
     public class FizzBuzzTest
     {
         [Test]
-        public void My_First_Test()
+        [TestCase("")]
+        [TestCase("pepe")]
+        public void Translate_NotANumber_ReturnsException(string input)
         {
-            Assert.That(false, Is.True);
+            Assert.Catch<ArgumentException>(() => FizzBuzzGenerator.Translate(input));
+        }
+
+        [Test]
+        [TestCase("0")]
+        [TestCase("101")]
+        public void Translate_MaxOrMinLimits_ReturnsException(string input)
+        {
+            Assert.Catch<ArgumentException>(() => FizzBuzzGenerator.Translate(input));
+        }
+
+        [Test]
+        [TestCase("3")]
+        public void Translate_MultipleOf3_ReturnsBuzz(string input)
+        {
+            Assert.That(FizzBuzzGenerator.Translate(input), Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        [TestCase("5")]
+        [TestCase("100")]
+        public void Translate_MultipleOf5_ReturnsBuzz(string input)
+        {
+            Assert.That(FizzBuzzGenerator.Translate(input), Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        [TestCase("15")]
+        public void Translate_MultipleOf3And5_ReturnsFizzBuzz(string input)
+        {
+            Assert.That(FizzBuzzGenerator.Translate(input), Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        [TestCase("1")]
+        [TestCase("2")]
+        public void Translate_OtherCases_ReturnsNumberAsString(string input)
+        {
+            Assert.That(FizzBuzzGenerator.Translate(input), Is.EqualTo(input));
         }
     }
 }
--- fizz-buzz/FizzBuzz/FizzBuzz.cs
+++ fizz-buzz/FizzBuzz/FizzBuzz.cs
@@ -1,8 +1,46 @@
-﻿using System;
+using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace FizzBuzz
 {
-    public class FizzBuzz
+    public class FizzBuzzGenerator
     {
+        public static string Translate(string input)
+        {
+            if (!IsNumber(input))
+                throw new ArgumentException("Invalid input: Please enter a natural number between 1 and 100.");
+
+            var intInput= int.Parse(input);
+
+            if (!IsInsideTheLimits(intInput))
+                throw new ArgumentException("Invalid input: Please enter a natural number between 1 and 100.");
+
+            if (IsMultipleOf(intInput, 3) && IsMultipleOf(intInput, 5))
+                return "FizzBuzz";
+
+            if (IsMultipleOf(intInput, 3))
+                return "Fizz";
+
+            if (IsMultipleOf(intInput, 5))
+                return "Buzz";
+
+            return intInput.ToString();
+        }
+
+
+        private static bool IsMultipleOf(int intInput, int multiple)
+        {
+            return intInput % multiple == 0;
+        }
+
+        private static bool IsInsideTheLimits(int input)
+        {
+            return input >= 1 && input <= 100;
+        }
+
+        private static bool IsNumber(string input)
+        {
+            return int.TryParse(input, out int number);
+        }
     }
 }
